f56eac0ba2ea69e0c230853b7e757b843a908cb2
Takashi Iwai <tiwai@suse.de>
2008-11-28 14:46:28 +0100

    ALSA: hda - Add quirk for Sony VAIO VGN-SR19XN
    Added model=sony-assamd for Sony VAIO VGN-SR19XN with ALC262 codec.
    Reference: Novell bnc#450080
    	https://bugzilla.novell.com/show_bug.cgi?id=450080
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- patch_realtek.c
+++ patch_realtek.c
@@ -63575,6 +63575,20 @@ comma
 id|SND_PCI_QUIRK
 c_func
 (paren
+l_int|0x104d
+comma
+l_int|0x9033
+comma
+l_string|&quot;Sony VAIO VGN-SR19XN&quot;
+comma
+"&t;&t;"
+id|ALC262_SONY_ASSAMD
+)paren
+comma
+"&t;"
+id|SND_PCI_QUIRK
+c_func
+(paren
 l_int|0x1179
 comma
 l_int|0x0001
