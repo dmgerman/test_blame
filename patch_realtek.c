0bb9ca7c27c31cf4f76cce01a074471eba2db9e5
Chris Bagwell <chris@cnpbagwell.com>
2009-01-01 10:32:08 +0100

    LSA: hda - Add HP Acacia detection
    Add automatic mapping of HP Acacia motherboards to 3stack-hp.  Allows
    for greater then 2 channel audio by enabling Channel Mode option in mixer.
    
    Motherboard specs:
    http://h10025.www1.hp.com/ewfrf/wc/document?docname=c01321559&lc=en&dlc=en&cc=us&product=3829353&os=2093&lang=en#
    
    Signed-off-by: Chris Bagwell <chris at cnpbagwell dot com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- patch_realtek.c
+++ patch_realtek.c
@@ -49665,6 +49665,19 @@ comma
 id|SND_PCI_QUIRK
 c_func
 (paren
+l_int|0x103c
+comma
+l_int|0x2a66
+comma
+l_string|&quot;HP Acacia&quot;
+comma
+id|ALC888_3ST_HP
+)paren
+comma
+"&t;"
+id|SND_PCI_QUIRK
+c_func
+(paren
 l_int|0x1043
 comma
 l_int|0x1873
