f3d3695fcfd84afb0c15053b982c461369f86d35
Takashi Iwai <tiwai@suse.de>
2009-02-18 13:16:26 +0100

    ALSA: hda - Add quirk for Acer X3200
    Acer X3200 needs model=auto, otherwise model=acer is pre-selected.
    
    Reference: Novell bnc#476268
    	https://bugzilla.novell.com/show_bug.cgi?id=476268
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- patch_realtek.c
+++ patch_realtek.c
@@ -49711,6 +49711,19 @@ c_func
 (paren
 l_int|0x1025
 comma
+l_int|0x0157
+comma
+l_string|&quot;Acer X3200&quot;
+comma
+id|ALC883_AUTO
+)paren
+comma
+"&t;"
+id|SND_PCI_QUIRK
+c_func
+(paren
+l_int|0x1025
+comma
 l_int|0x0158
 comma
 l_string|&quot;Acer AX1700-U3700A&quot;
