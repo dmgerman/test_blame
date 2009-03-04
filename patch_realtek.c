17db7033259effbf60361367ac177af14aff4252
Herton Ronaldo Krzesinski <herton@mandriva.com.br>
2009-03-04 14:22:52 -0300

    ALSA: hda - Map 3stack-hp model (ALC888) for HP Educ.ar
    Added model=3stack-hp for HP Educ.ar desktop machine (103c:2a72).
    
    Signed-off-by: Herton Ronaldo Krzesinski <herton@mandriva.com.br>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- patch_realtek.c
+++ patch_realtek.c
@@ -50515,6 +50515,19 @@ comma
 id|SND_PCI_QUIRK
 c_func
 (paren
+l_int|0x103c
+comma
+l_int|0x2a72
+comma
+l_string|&quot;HP Educ.ar&quot;
+comma
+id|ALC888_3ST_HP
+)paren
+comma
+"&t;"
+id|SND_PCI_QUIRK
+c_func
+(paren
 l_int|0x1043
 comma
 l_int|0x1873
