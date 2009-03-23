bbeacf42221ae042590001641e5dfe53201d7df9
Takashi Iwai <tiwai@suse.de>
2009-03-23 22:50:52 +0100

    ALSA: hda - Add quirk for Acer Ferrari 5000
    Add a quirk model=acer-aspire for Acer Ferrari 5000 with ALC883 codec.
    Note that model=auto doesn't work for this laptop because of broken BIOS
    (that doesn't set the subsystem id properly).
    
    Tested-by: Russ Dill <russ.dill@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- patch_realtek.c
+++ patch_realtek.c
@@ -50398,6 +50398,19 @@ c_func
 (paren
 l_int|0x1025
 comma
+l_int|0x010a
+comma
+l_string|&quot;Acer Ferrari 5000&quot;
+comma
+id|ALC883_ACER_ASPIRE
+)paren
+comma
+"&t;"
+id|SND_PCI_QUIRK
+c_func
+(paren
+l_int|0x1025
+comma
 l_int|0x0110
 comma
 l_string|&quot;Acer Aspire&quot;
