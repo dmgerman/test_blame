5f208813eccc5fd9b2dfb4fd14437c2263e01434
Justin P. Mattock <justinmattock@gmail.com>
2010-06-06 16:09:51 -0700

    ALSA: hda - Add SSID table for MacBookAir1,1
    This patch add's the MacBookAir1,1 SSID entry to
    patch_realtek.c which adds sound support.
    bug entry:
        https://bugs.launchpad.net/mactel-support/+bug/268301
    
    Note:I do not have this machine on hand only
    codec#0 file for the machine so please
    test if you have the appropriate equipment.
    
    Signed-off-by: Justin P. Mattock <justinmattock@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- patch_realtek.c
+++ patch_realtek.c
@@ -55348,6 +55348,19 @@ c_func
 (paren
 l_int|0x106b
 comma
+l_int|0x3400
+comma
+l_string|&quot;MacBookAir 1,1&quot;
+comma
+id|ALC885_MBP3
+)paren
+comma
+"&t;"
+id|SND_PCI_QUIRK
+c_func
+(paren
+l_int|0x106b
+comma
 l_int|0x3500
 comma
 l_string|&quot;MacBookAir 2,1&quot;
