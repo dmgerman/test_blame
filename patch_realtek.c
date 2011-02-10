08f85d1ad85963d97f60e8a6a5f5e15faf4ac87a
Anisse Astier <anisse@astier.eu>
2011-02-10 13:14:44 +0100

    ALSA: hda - add quirk for Ordissimo EVE using a realtek ALC662
    This netbook has a only one jack output and an internal mic.
    
    By default, mic and jack sense aren't working. Using lenovo-101e
    parameters makes both work.
    
    The device seems based on a Sharetronic Q70, so this should fix audio for
    this model too.
    
    Signed-off-by: Anisse Astier <anisse@astier.eu>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- patch_realtek.c
+++ patch_realtek.c
@@ -113296,6 +113296,19 @@ id|ALC663_ASUS_H13
 )paren
 comma
 "&t;"
+id|SND_PCI_QUIRK
+c_func
+(paren
+l_int|0x1991
+comma
+l_int|0x5628
+comma
+l_string|&quot;Ordissimo EVE&quot;
+comma
+id|ALC662_LENOVO_101E
+)paren
+comma
+"&t;"
 (brace
 )brace
 )brace
