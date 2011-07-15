4b570b05034c865bd52c0c9489c0cdba27f414d0
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-15 12:38:28 -0400

    sound: Add module.h to the previously silent sound users
    Lots of sound drivers were getting module.h via the implicit presence
    of it in <linux/device.h> but we are going to clean that up.  So
    fix up those users now.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- patch_realtek.c
+++ patch_realtek.c
@@ -3,6 +3,7 @@ macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &lt;linux/delay.h&gt;
 macro_line|#include &lt;linux/slab.h&gt;
 macro_line|#include &lt;linux/pci.h&gt;
+macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;sound/core.h&gt;
 macro_line|#include &lt;sound/jack.h&gt;
 macro_line|#include &quot;hda_codec.h&quot;
