bfa06f83db1919a8ddcac29fc9c06367284ab1b4
Ebben Aries <earies@dscp.org>
2013-05-29 22:27:18 -0600

    ALSA: hda - add dock support for Thinkpad T431s
    Add a model/fixup string "lenovo-dock", for Thinkpad T431s, to allow sound in docking station.
    
    Tested on Lenovo T431s with ThinkPad Mini Dock Plus Series 3
    
    Signed-off-by: Ebben Aries <earies@dscp.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- patch_realtek.c
+++ patch_realtek.c
@@ -19548,6 +19548,19 @@ c_func
 (paren
 l_int|0x17aa
 comma
+l_int|0x2208
+comma
+l_string|&quot;Thinkpad T431s&quot;
+comma
+id|ALC269_FIXUP_LENOVO_DOCK
+)paren
+comma
+"&t;"
+id|SND_PCI_QUIRK
+c_func
+(paren
+l_int|0x17aa
+comma
 l_int|0x2203
 comma
 l_string|&quot;Thinkpad X230 Tablet&quot;
